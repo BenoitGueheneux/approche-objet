915a0cfbad04172f955e81469b1418d7f54da535
Benoit Gueheneux <benoit.gueheneux@hotmail.com>
2023-04-11 17:11:26 +0200

    .

--- src/entites/AdressePostale.java
+++ src/entites/AdressePostale.java
@@ -5,5 +5,18 @@ public class AdressePostale {
 	String libelleRue;
 	int codePostal;
 	String ville;
+	
+	public AdressePostale() {
+		
+	}
+	
+	public AdressePostale(int numeroRue,String libelleRue,int codePostal,String ville) {
+		this.numeroRue = numeroRue;
+		this.libelleRue =libelleRue;
+		this.codePostal = codePostal;
+		this.ville = ville;
+		
+		
+	}
 
 }
--- src/entites2/Personne.java
+++ src/entites2/Personne.java
@@ -6,4 +6,13 @@ public class Personne {
 	public String nom;
 	public String prenom;
 	public AdressePostale adresse_postale;
+	public Personne(String nom,String prenom) {
+		this.nom = nom;
+		this.prenom = prenom;
+	}
+	public Personne(String nom,String prenom,AdressePostale adresse_postale) {
+		this.nom = nom;
+		this.prenom = prenom;
+		this.adresse_postale = adresse_postale;
+	}
 }
