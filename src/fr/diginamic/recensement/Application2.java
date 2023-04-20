104642e641e26dfbc3d910b3a29bff159d89b7ff
Benoit Gueheneux <benoit.gueheneux@hotmail.com>
2023-04-20 16:37:34 +0200

    TP 16 ajout du while et completion de l'affichage

--- src/fr/diginamic/recensement/Application2.java
+++ src/fr/diginamic/recensement/Application2.java
@@ -3,16 +3,25 @@ package fr.diginamic.recensement;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class Application {
+public class Application2 {
 
 	public static void main(String[] args) throws IOException {
 		Recensement recensement = new Recensement();
+		int choix =0;
+		do {
 		Scanner monNombre = new Scanner(System.in);
 		System.out.println("1. Population d’une ville donnée");
 		System.out.println("2. Population d’un département donné");
+		System.out.println("3. Population d’une région donnée");
+		System.out.println("4. Afficher les 10 régions les plus peuplées");
+		System.out.println("5. Afficher les 10 départements les plus peuplés");
+		System.out.println("6. Afficher les 10 villes les plus peuplées d’un département");
+		System.out.println("7. Afficher les 10 villes les plus peuplées d’une région");
+		System.out.println("8. Afficher les 10 villes les plus peuplées de France");
+		System.out.println("9. Sortir");
 		System.out.println("Enter number from 0 to 9");
 
-		int choix = Integer.parseInt(monNombre.next());
+		choix = Integer.parseInt(monNombre.next());
 		Scanner scanner = new Scanner(System.in);
 		switch (choix) {
 		case 1:
@@ -51,10 +60,9 @@ public class Application {
 			RechercheTop10VilleLesPlusPeuplees recherche8 = new RechercheTop10VilleLesPlusPeuplees();
 			recherche8.traiter(recensement, scanner);
 			break;
-		case 9:
-			break;
+		}} while(choix!=9);
 	}
 
 	}
 
-}
+
